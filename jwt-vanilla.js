d2dfd27ea2ee97052bc61e8011b3ea7c83869ae0
Jade D. Kharats <jade.kharats@gmail.com>
2020-05-03 11:16:10 +0200

    refacto login

--- jwt-vanilla.js
+++ jwt-vanilla.js
@@ -1,77 +1,50 @@
-function getToken() {
+function login() {
 
-    var xhr = new XMLHttpRequest();
-    var userElement = document.getElementById('username');
-    var passwordElement = document.getElementById('password');
-    var platformElement = document.getElementById('platform');
-    var errorElement = document.getElementById('error');
-    var loginElement = document.getElementById('login');
-    var logoutElement = document.getElementById('logout');
-    var user = userElement.value;
-    var password = passwordElement.value;
-    var platform = platformElement.value;
-
-    localStorage.setItem('bom_url', platform);
-
-    var loginUrl = platform + "/api/sign_ins"
-  
-    xhr.open('POST', loginUrl, true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.addEventListener('load', function() {
-      var responseObject = JSON.parse(this.response);
-      console.log(responseObject);
-      if (responseObject.token) {
-        localStorage.setItem('token', responseObject.token);
-        loginElement.classList.add("hidden");
-        logoutElement.classList.remove("hidden");
-        // getMoods();
-        setInterval(getMoods, 3000);        
-      } else {
-        errorElement.innerHTML = "No token received";
-      }
-    });
-  
-    var sendObject = JSON.stringify({user:{email: user, password: password}});
-  
-    console.log('going to send', sendObject);
-  
-    xhr.send(sendObject);
-  }
-
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', getLoginUrl(), true);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  xhr.addEventListener('load', function() {
+    var responseObject = JSON.parse(this.response);
+    console.log(responseObject);
+    if (responseObject.token) {
+      successLogin(responseObject.token);        
+    } else {
+      failLogin();      
+    }
+  });
+  var sendObject = jsonLoginObject();
+  xhr.send(sendObject);
+}
 
 function getMoods() {
-
-    var MoodUrl = localStorage.getItem('bom_url') + "/api/moods"
-    var xhr = new XMLHttpRequest();
-    var wrapperElement = document.getElementById('wrapper');
-    
-    xhr.open('GET', MoodUrl, true);
-    xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('token'));
-    xhr.addEventListener('load', function() {
-      var responseObject = JSON.parse(this.response);
-      console.log(responseObject);
-      wrapperElement.innerHTML="";
-      responseObject.forEach(function(item,i){
-        console.log(item);
-        addTimeCard(item);
-      });
+  var xhr = new XMLHttpRequest();
+  
+  
+  xhr.open('GET', getMoodsUrl(), true);
+  xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('token'));
+  xhr.addEventListener('load', function() {
+    var responseObject = JSON.parse(this.response);
+    clearWrapper();
+    responseObject.forEach(function(item,i){
+      console.log(item);
+      addTimeCard(item);
     });
+  });
 
-    
-    xhr.send();
-  }
+  
+  xhr.send();
+}
+
 
   function addTimeCard(item) {
     var wrapperElement = document.getElementById('wrapper');
     var box = document.createElement("div");
-    var card = document.createElement("div");
     var content = document.createElement("div");
     var user = document.createElement("h4");
     var comment = document.createElement("p");
 
     box.classList.add("box");
     box.classList.add("box" + item.level);
-    card.classList.add("card");
     content.classList.add("content");
 
     user.textContent = item.user;
@@ -84,7 +57,51 @@ function getMoods() {
     wrapperElement.insertBefore(box, wrapperElement.firstChild);  
   }
 
-  function logout(){
-    localStorage.clear();
-    location.reload();
-  }
+// Post Refacto , function with 10 lines max
+
+function logout(){
+  localStorage.clear();
+  location.reload();
+}
+
+function hiddenLogin() {
+  var loginElement = document.getElementById('login');
+  var logoutElement = document.getElementById('logout');
+  loginElement.classList.add("hidden");
+  logoutElement.classList.remove("hidden");
+}
+
+function successLogin(token) {
+  localStorage.setItem('token', token);
+  hiddenLogin();
+  getMoods();
+  // setInterval(getMoods, 3000);
+}
+
+function failLogin() {
+  var errorElement = document.getElementById('error');
+  errorElement.innerHTML = "No token received";
+}
+
+function jsonLoginObject(){
+  var user = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  return JSON.stringify({user:{email: user, password: password}});
+}
+
+function getLoginUrl() {
+  var platform = document.getElementById('platform').value;
+  localStorage.setItem('bom_url', platform);
+  var loginUrl = platform + "/api/sign_ins";
+  return loginUrl;
+}
+
+function getMoodsUrl() {
+  var MoodUrl = localStorage.getItem('bom_url') + "/api/moods";
+  return MoodUrl;
+}
+
+function clearWrapper() {
+  var wrapperElement = document.getElementById('wrapper');
+  wrapperElement.innerHTML="";
+}
